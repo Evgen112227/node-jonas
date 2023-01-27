b3cac04ecd7ef5f4d9a41f03f601428b9b9381cb
Yauheni Hladki <Deribasovich@gmail.com>
2023-01-27 17:48:24 +0300

    refactoring routes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,64 @@
 const Tour = require('./../models/tourModel');
 
+exports.alliasTopTour = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage, price';
+  req.query.fields = 'name,price,ratingsAverage,difficulty';
+  next();
+};
+
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //Advanced Filtering
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    let query = Tour.find(JSON.parse(queryString));
+    //Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      //default query
+      query = query.sort('price');
+    }
+
+    //Field limiting, to reduce unnecessary fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      //excluding field
+      query = query.select('-__v');
+    }
+
+    //Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
